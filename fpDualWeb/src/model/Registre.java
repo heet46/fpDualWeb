b8a1c0efa4ec2e5aeaac096d8b6ecc8de29424f2
JOAN ESPUNYES <joanespunyes@hotmail.com>
2017-06-27 11:24:36 +0200

    hores

--- fpDualWeb/src/model/Registre.java
+++ fpDualWeb/src/model/Registre.java
@@ -6,4 +6,29 @@ public class Registre {
 	private int hores;
 	private Alumne alumne;
 	private Activitat activitat;
+	
+	public String getData() {
+		return data;
+	}
+	public void setData(String data) {
+		this.data = data;
+	}
+	public int getHores() {
+		return hores;
+	}
+	public void setHores(int hores) {
+		this.hores = hores;
+	}
+	public Alumne getAlumne() {
+		return alumne;
+	}
+	public void setAlumne(Alumne alumne) {
+		this.alumne = alumne;
+	}
+	public Activitat getActivitat() {
+		return activitat;
+	}
+	public void setActivitat(Activitat activitat) {
+		this.activitat = activitat;
+	}
 }
--- fpDualWeb/src/servlet/ComprovarData.java
+++ fpDualWeb/src/servlet/ComprovarData.java
@@ -1,15 +1,20 @@
 package servlet;
 
 import java.io.IOException;
+import java.sql.SQLException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import model.Registre;
+import model.RegistreDAO;
 
 @WebServlet("/ComprovarData")
 public class ComprovarData extends HttpServlet {
@@ -24,21 +29,36 @@ public class ComprovarData extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
+		SimpleDateFormat DATE_FORMAT2 = new SimpleDateFormat("dd/MM/yyyy");
+		
 		String data = request.getParameter("data");
-		System.out.println("Data que arriba:"+data);
 		
-		SimpleDateFormat formateador = new SimpleDateFormat("yyyy-MM-dd");
-		Date data2 = null;
-		try{
-		   data2 = formateador.parse(data);
+		Date date;
+		String data2 = null;
+		try {
+			date = DATE_FORMAT2.parse(data);
+			data2 = DATE_FORMAT.format(date);
+		} catch (ParseException e) {
+			e.printStackTrace();
 		}
-		catch(ParseException e){
-		   // Error, la cadena de texto no se puede convertir en fecha.
-		}
-		System.out.println("Data canviada:"+data2.toString());
-		String data3 = formateador.format(data2);
 		
-		System.out.println("Data canviada2:"+data3);
+		Registre registre = new Registre();
+		RegistreDAO rDAO = new RegistreDAO();
+		registre.setData(data2);
+		List<Object> llista;
+		try {
+			llista = rDAO.valorsData(registre);
+			System.out.println(llista.toString());
+			if(llista.isEmpty()){
+				System.out.println("No hi ha res al registre");
+			}else{
+				System.out.println("Hi ha cosetes");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
 
 
 		
