6509fbf93dfa774939fb50afc2302602fecbccaa
fnavarrov <fnavarrov@FNAVARROV00W7.indra.es>
2017-06-28 09:58:51 +0200

    registreHores

--- fpDualWeb/src/model/RegistreDAO.java
+++ fpDualWeb/src/model/RegistreDAO.java
@@ -10,25 +10,26 @@ import controlador.GestorDB;
 
 public class RegistreDAO {
 	GestorDB gestorDB;
-	
-	public RegistreDAO(){
+
+	public RegistreDAO() {
 		gestorDB = new GestorDB(Constants.SERVER, Constants.PORT, Constants.BD);
 	}
-	
-	public void afegirRegistre(Registre reg) throws SQLException{
-		String consultaSQL = "INSERT INTO registre VALUES("+reg.getActivitat().getId()+","+reg.getAlumne().getIdUsuari()+",'"+reg.getData()+"',"+reg.getHores()+")";
+
+	public void afegirRegistre(Registre reg) throws SQLException {
+		String consultaSQL = "INSERT INTO registre VALUES(" + reg.getActivitat().getId() + ","
+				+ reg.getAlumne().getIdUsuari() + ",'" + reg.getData() + "'," + reg.getHores() + ")";
 		System.out.println(consultaSQL);
 		gestorDB.modificarRegistre(consultaSQL);
 	}
-	
-	public List<Object> valorsData(Registre reg) throws SQLException{
+
+	public List<Object> valorsData(Registre reg) throws SQLException {
 		List<Object> valors = new ArrayList<Object>();
 		ResultSet rs = null;
-		String consultaSQL = "SELECT * FROM registre WHERE data='"+reg.getData()+"'";
-		
+		String consultaSQL = "SELECT * FROM registre WHERE data='" + reg.getData() + "'";
+
 		rs = gestorDB.consultaRegistres(consultaSQL);
-		
-		while(rs.next()){
+
+		while (rs.next()) {
 			valors.add(rs.getInt(1));
 			valors.add(rs.getInt(2));
 			valors.add(rs.getDate(3));
@@ -37,51 +38,58 @@ public class RegistreDAO {
 
 		return valors;
 	}
-	
-	public List<String> llistaActivitats() throws SQLException{
+
+	public List<String> llistaActivitats() throws SQLException {
 		List<String> llista = new ArrayList<String>();
 		ResultSet rs = null;
 		String consultaSQL = "SELECT DISTINCT codi FROM activitat";
-		
+
 		rs = gestorDB.consultaRegistres(consultaSQL);
-		
-		while(rs.next()){
+
+		while (rs.next()) {
 			llista.add(rs.getString(1));
 		}
 		return llista;
 	}
-	
-	public int sumaHores(Registre reg) throws SQLException{
+
+	public int sumaHores(Registre reg) throws SQLException {
 		int suma = 0;
 		ResultSet rs = null;
-		String consultaSQL = "SELECT SUM(r.hores) "
-				+ "FROM registre AS r "
-				+ "WHERE r.data LIKE '"+reg.getData()+"' AND r.id_alumne LIKE "+reg.getAlumne().getIdUsuari();
-		
+		String consultaSQL = "SELECT SUM(r.hores) " + "FROM registre AS r " + "WHERE r.data LIKE '" + reg.getData()
+				+ "' AND r.id_alumne LIKE " + reg.getAlumne().getIdUsuari();
+
 		rs = gestorDB.consultaRegistres(consultaSQL);
-		while(rs.next()){
+		while (rs.next()) {
 			suma = rs.getInt(1);
 		}
 		return suma;
 	}
-	
-	public int idActivitat(String codi) throws SQLException{
+
+	public int idActivitat(String codi) throws SQLException {
 		int id = 0;
 		ResultSet rs = null;
-		String consultaSQL = "SELECT a.id_activitat "
-				+ "FROM activitat AS a "
-				+ "WHERE a.codi='"+codi+"'";
+		String consultaSQL = "SELECT a.id_activitat " + "FROM activitat AS a " + "WHERE a.codi='" + codi + "'";
 		System.out.println(consultaSQL);
 		rs = gestorDB.consultaRegistres(consultaSQL);
-		
-		while(rs.next()){
+
+		while (rs.next()) {
 			id = rs.getInt(1);
 		}
-		
+
 		return id;
 	}
-		
-	public void tancarConn(){
+
+	public ResultSet consultarHores(String consul) throws SQLException {
+		int id = 0;
+		ResultSet rs = null;
+		String consultaSQL = "SELECT * FROM registre WHERE" + consul + ";";
+		System.out.println(consultaSQL);
+		rs = gestorDB.consultaRegistres(consultaSQL);
+
+		return rs;
+	}
+
+	public void tancarConn() {
 		gestorDB.tancarConnexio();
 	}
 }
